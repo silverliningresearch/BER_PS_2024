9669c53e18c471ec349b4d48dfd22047458ba3e7
lexuanbinh <lb@silverliningresearch.com>
2024-09-17 23:55:34 +0700

    update files

--- scripts/postal_code_search_q27.js
+++ scripts/postal_code_search_q27.js
@@ -119,24 +119,3 @@ function hidePostalCodeSection_q27() {
   $('#inputPostalCodeQ27ID').hide();
 }
 
-/* function waitForElement(querySelector, timeout){
-  return new Promise((resolve, reject)=>{
-    var timer = false;
-    if(document.querySelectorAll(querySelector).length) return resolve();
-    const observer = new MutationObserver(()=>{
-      if(document.querySelectorAll(querySelector).length){
-        observer.disconnect();
-        if(timer !== false) clearTimeout(timer);
-        return resolve();
-      }
-    });
-    observer.observe(document.body, {
-      childList: true, 
-      subtree: true
-    });
-    if(timeout) timer = setTimeout(()=>{
-      observer.disconnect();
-      reject();
-    }, timeout);
-  });
-} */
